9d52c3ee440021f65d92440ca091a8d74ea405d3
Mark Foskett <markryanfoskett@gmail.com>
2023-05-14 22:34:12 +1000

    Add project files.

---
+++ expense-tracker/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace expense_tracker.Models
+{
+    public class Category
+    {
+        // Category Key
+        [Key]
+        public int CategoryId { get; set; }
+        // Category Columns
+        [Column(TypeName  = "nvarchar(50)")]
+        [Required(ErrorMessage = "Title is required.")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string Icon { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string Type { get; set; } = "Expense";
+    }
+}
